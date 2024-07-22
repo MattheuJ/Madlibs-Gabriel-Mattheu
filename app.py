88a2b2b72bc9318bd44fc265743babb1119ba7fa
MattheuJ <mattheujimenez@gmail.com>
2024-07-22 12:30:45 -0700

    Got submission working

--- app.py
+++ app.py
@@ -5,34 +5,21 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['GET','POST'])
 def mablibsApp():
-    if request.method == 'POST':
-        dayoftheweek = request.form['dayoftheweek']
-        noun1 = request.form['noun1']
-        place1 = request.form['place1']
-        pasttencever1 = request.form['pasttenceverb1']
-        income = request.form['income']
-        noun2 = request.form['noun2']
-        place2 = request.form['place2']
-        pasttenceverb2 = request.form['pasttenceverb2']
-    return render_template('index.html')
-    "On one {day of the week}"
-
-@app.route('/', methods = ['GET', 'POST'])
-def mablibsApp():
-    return render_template('index.html', url=url_for('madlibscompleted'))
+    return render_template('index.html',url=url_for('madlibscompleted'))
 
 @app.route('/completed', methods = ['GET', 'POST'])
 def madlibscompleted():
-    return render_template('completed.html')
-
-
-
-
-#print(f"On one {y}, a man named {name} went to {placeList[random.randint(0,2)]}. 
-#      He's retired now, but he {job} for a living, and made ${income} a year. 
-#      Now, in his freetime, he likes to dress up as a {dressUpList[random.randint(0,2)]}. 
-#      He likes to go to {dressUpList[random.randint(0,2)]} coneventions, which are always hosted in {placeList[random.randint(0,2)]} for some reason. 
-#      Last year, at one of the conventions, he was arrested because he {arrest} another convestion goer becuase they were dressed as a {dressUpList[random.randint(0,2)]}.")
+    if request.method == 'POST':
+        dayoftheweek = request.form['dayoftheweek']
+        name = request.form['name']
+        place1 = request.form['place1']
+        pasttenseverb1 = request.form['pasttenseverb1']
+        income = request.form['income']
+        noun1 = request.form['noun1']
+        place2 = request.form['place2']
+        pasttenseverb2 = request.form['pasttenseverb2']
+        noun2 = request.form['noun2']
+    return render_template('completed.html',url=url_for('madlibscompleted'), dayoftheweek=dayoftheweek, name=name, place1=place1, pasttenseverb1=pasttenseverb1, income=income, noun1=noun1, place2=place2, pasttenceverb2=pasttenseverb2, noun2=noun2)
 
 
 if __name__ == '__main__':
